02715792524eeb62df91614c51692c177ee25080
lizyweb <147726740+lizyweb@users.noreply.github.com>
2024-01-24 17:13:48 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  // Sticky Navbar
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+      $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+  } else {
+      $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+  }
+});
+
+});
